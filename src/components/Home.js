491fe9223e0cf60a14d11b4aaf0bf119afe5a836
Muriithi Ian <33234188+muriithi-ian@users.noreply.github.com>
2021-05-16 00:59:10 +0300

    Added personal description to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,8 +10,8 @@ export default function Home() {
         className="fixed object-cover w-full h-full"
       />
       <section className="fixed flex justify-center min-h-screen  lg:pt-64 px-8">
-        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
-          Aloha. I'm Muriithi Ian.
+        <h1 className="text-4xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Aloha. I'm Muriithi Ian. An Innovative Programmer and Internet Entrepreneur striving to unify the world through code. A creative thinker, proficient in software development and working with technologies for over
         </h1>
       </section>
     </main>
