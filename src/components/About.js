e18e0d9a2ea3783f2d3f00cbc992ebb602a82a41
Muriithi Ian <imuriithiian@gmail.com>
2022-09-19 23:45:35 +0300

    Added responsive design

--- src/components/About.js
+++ src/components/About.js
@@ -32,24 +32,34 @@ export default function About() {
 
 
   return (
-    <main className="relative">
-      <img src={workspace} alt="Work Space" className="fixed w-full" />
-      <div className="p-10 lg:pt-48 container mx-auto relative">
-        {!author ? <ClipLoader color="#38A169" css={override} size={150} /> : <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
-          <img
-            src={urlFor(author.authorImage).url()}
-            className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
-            alt="Muritihi Ian"
-          />
-          <div className="text-lg flex flex-col justify-center">
-            <h1 className="cursive text-6xl text-green-300 mb-4">
-              Hey there. I'm{" "}
-              <span className="text-green-100">{author.name}</span>
-            </h1>
-            <p className="text-green-200 text-lg overflow">{author.bio}</p>
-          </div>
-        </section>
-        }</div>
-    </main>
-  );
+		<main className="relative">
+			<img
+				src={workspace}
+				alt="Work Space"
+				className="fixed object-cover w-full h-full"
+			/>
+			<div className="p-10 lg:pt-48 container mx-auto relative">
+				{!author ? (
+					<ClipLoader color="#38A169" css={override} size={150} />
+				) : (
+					<section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+						<img
+							src={urlFor(author.authorImage).url()}
+							className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
+							alt="Muritihi Ian"
+						/>
+						<div className="text-lg flex flex-col justify-center">
+							<h1 className="cursive text-6xl text-green-300 mb-4">
+								Hey there. I'm{" "}
+								<span className="text-green-100">{author.name}</span>
+							</h1>
+							<p className="text-green-200 text-lg overflow-auto">
+								{author.bio}
+							</p>
+						</div>
+					</section>
+				)}
+			</div>
+		</main>
+	);
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,17 @@ import workspace from "../workspace.jpg";
 
 export default function Home() {
   return (
-    <main>
-      <img
-        src={workspace}
-        alt="Work Space"
-        className="fixed object-cover w-full h-full"
-      />
-      <section className="fixed flex justify-center min-h-screen  lg:pt-64 px-8">
-        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
-          Aloha. I'm Muriithi Ian.
-        </h1>
-      </section>
-    </main>
-  );
+		<main>
+			<img
+				src={workspace}
+				alt="Work Space"
+				className="fixed object-cover w-full h-full"
+			/>
+			<section className="fixed flex items-center justify-center min-h-screen  lg:pt-64 px-8">
+				<h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+					Aloha. I'm Muriithi Ian.
+				</h1>
+			</section>
+		</main>
+	);
 }
